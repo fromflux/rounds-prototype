da602b297cfe2b8894665257789dee58f4c794ad
Rod Melo <rodmelo@fromflux.com>
2018-03-16 20:58:06 +0000

    Updated product images

--- productsData.js
+++ productsData.js
@@ -1,101 +1,101 @@
 module.exports = [{
   id: 1,
   name: "ADNAMS, LONGBOAT",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/1.png.asset/1480951193105.png",
+  image_url: "img/ADNAMS-LONGBOAT.png",
   price: 4.30
 },{
   id: 2,
   name: "BAD CO, WHITER SHADE OF PALE",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/2.png.asset/1480951199662.png",
+  image_url: "img/BAD CO-WHITER SHADE OF PALE.jpg",
   price: 4.75
 },{
   id: 3,
   name: "BAYS, GOLD",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/3.png.asset/1480951205279.png",
+  image_url: "img/BAYS-GOLD.png",
   price: 4.75
 },{
   id: 4,
   name: "BRAINS, CAFE PHOENIX",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/4.png.asset/1480951213517.png",
+  image_url: "img/BRAINS-CAFE PHOENIX.png",
   price: 4.45
 },{
   id: 5,
   name: "COTLEIGH, TAWNY",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/5.png.asset/1480951219437.png",
+  image_url: "img/COTLEIGH-TAWNY.jpg",
   price: 4.25
 },{
   id: 6,
   name: "EVERARDS, SLY FOX",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/6.png.asset/1480951226872.png",
+  image_url: "img/EVERARDS-SLY FOX.jpg",
   price: 4.75
 },{
   id: 7,
   name: "GREAT HECK, DAVE",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/7.png.asset/1480951234048.png",
+  image_url: "img/GREAT HECK-DAVE.jpg",
   price: 4.75
 },{
   id: 8,
   name: "GREAT YORKSHIRE, TOP OF THE HOPS 2017",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/8.png.asset/1480951238759.png",
+  image_url: "img/GREAT YORKSHIRE-TOP OF THE HOPS 2017.jpg",
   price: 4.50
 },{
   id: 9,
   name: "GREENE KING, MIGHTY MOOSE",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/9.png.asset/1480951245613.png",
+  image_url: "img/GREENE KING-MIGHTY MOOSE.png",
   price: 4.30
 },{
   id: 10,
   name: "HARBOUR, BREAKFAST PALE",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/10.png.asset/1480951253258.png",
+  image_url: "img/HARBOUR-BREAKFAST PALE.jpg",
   price: 4.75
 },{
   id: 11,
   name: "HOP BACK, WINTER LIGHTNING",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/11.png.asset/1480951258510.png",
+  image_url: "img/HOP BACK-WINTER LIGHTNING.png",
   price: 4.20
 },{
   id: 12,
   name: "ILKLEY, MARY JANE",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/12.png.asset/1480951264359.png",
+  image_url: "img/ILKLEY-MARY JANE.png",
   price: 4.75
 },{
   id: 13,
   name: "KNOPS, MUSSELBURGH BROKE",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/13.png.asset/1480951270055.png",
+  image_url: "img/KNOPS-MUSSELBURGH BROKE.png",
   price: 4.75
 },{
   id: 14,
   name: "ORKNEY, DARK ISLAND",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/14.png.asset/1480951274775.png",
+  image_url: "img/ORKNEY-DARK ISLAND.gif",
   price: 4.10
 },{
   id: 15,
   name: "ROOSTERS, LITTLE BIRD",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/15.png.asset/1480951279905.png",
+  image_url: "img/ROOSTERS-LITTLE BIRD.jpg",
   price: 4.30
 },{
   id: 16,
   name: "SALTAIRE, BLONDE",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/16.png.asset/1480951288109.png",
+  image_url: "img/SALTAIRE-BLONDE.jpg",
   price: 4.75
 },{
   id: 17,
   name: "SHARP’S SEA FURY",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/17.png.asset/1480951301248.png",
+  image_url: "img/SHARPS SEA FURY.jpg",
   price: 4.10
 },{
   id: 18,
   name: "SKINNERS, CORNISH TRAWLER",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/18.png.asset/1480951306144.png",
+  image_url: "img/SKINNERS-CORNISH TRAWLER.jpg",
   price: 4.80
 },{
   id: 19,
   name: "TICKETY BREW, SESSION IPA",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/19.png.asset/1480951323293.png",
+  image_url: "img/TICKETY BREW-SESSION IPA.jpg",
   price: 4.95
 },{
   id: 20,
   name: "WOODFORDES, TUNDRA",
-  image_url: "http://www.nicholsonspubs.co.uk/content/dam/nicholsons/images/content/ales/winter/2016/20.png.asset/1480951329525.png",
+  image_url: "img/WOODFORDES-TUNDRA.jpg",
   price: 4.15
 }]
